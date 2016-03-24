3dfa3e37f6a0631a2f673969cbb1cf370b1b92bc
finbel <oregan@kth.se>
2016-03-24 08:31:25 +0100

    "login added"

--- avalon/urls.py
+++ avalon/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.contrib.auth.views import login
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include('blog.urls')),
+    url(r'^accounts/login/$', login, {'template_name': 'login.html'})
 ]
