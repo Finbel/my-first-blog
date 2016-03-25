d0f7f55afb39bfa1599f8a068bcb64042ba868df
finbel <oregan@kth.se>
2016-03-25 10:52:35 +0100

    Added drafts, fixed login redirect

--- blog/views.py
+++ blog/views.py
@@ -22,7 +22,6 @@ def post_new(request):
 		if form.is_valid():
 			post = form.save(commit=False)
 			post.author = request.user
-			post.published_date = timezone.now()
 			post.save()
 			return redirect('post_detail', pk=post.pk)	
 	else:
@@ -33,12 +32,27 @@ def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
         form = PostForm(request.POST, instance=post)
+        date = post.published_date
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = get_object_or_404(Post, pk=pk).published_date
+            post.published_date = date
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_draft_list(request):
+	posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+	return render(request, 'blog/post_draft_list.html', {'posts': posts})
+
+def post_publish(request, pk):
+	post = get_object_or_404(Post, pk=pk)
+	post.publish()
+	return redirect('blog.views.post_detail', pk=pk)
+
+def post_remove(request, pk):
+	post = get_object_or_404(Post, pk=pk)
+	post.delete()
+	return redircet('blog.views.post_list')
