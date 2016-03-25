224a0c00714b2daf65bc74de6fd576d024cdf5fa
finbel <oregan@kth.se>
2016-03-25 12:20:53 +0100

    fixed login/logout

--- avalon/urls.py
+++ avalon/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
-from django.contrib.auth.views import login
+from django.contrib.auth.views import login, logout
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include('blog.urls')),
-    url(r'^accounts/login/$', login, {'template_name': 'blog/login.html'})
+    url(r'^accounts/login/$', login, {'template_name': 'blog/login.html'}),
+    url(r'^accounts/logout/$', logout, {'next_page': '/'}),
 ]
